878f4c2620f3eaa3a282bb217fe9fa7da2c100fc
quested100 <25rupalk@retsdstudent.ca>
2025-02-16 17:57:13 +0000

    basic features  working

--- src/main/java/PizzaTime.java
+++ src/main/java/PizzaTime.java
@@ -1,8 +1,24 @@
-public class PizzaTime {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class PizzaTime {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args){
-        System.out.println("Pizza Time!");
+        streetName();
+    }
+    private static void streetName(){
+        System.out.println("What street do you live on");
+        String StreetName = scanner.nextLine();
+
+        ArrayList<String> address = new ArrayList<String>();
+        address.add("Burrows");
+        address.add("Main");
+        address.add("Henderson");
+        address.add("Magnus");
+        address.add("Mountain");
+        System.out.println(address);
         
     }
+    
 }
